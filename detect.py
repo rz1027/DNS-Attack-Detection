39d1895263df847ca1a779c3c38462f65fe6c759
ryanalam <73023850+ryanalam@users.noreply.github.com>
2022-09-29 20:03:47 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,1 +1,20 @@
+from scapy.all import IP,DNS, sniff, DNSQR, sr1
+import sys
 
+try :
+    interface = input ("[*] Enter desired Interface : ")
+except:
+    KeyboardInterrupt
+    print("[*]User Requested Shutdown")
+    print("[*]Exiting...")
+    sys.exit(1)
+
+def querysnif(pkt):
+    if IP in pkt:
+        ip_src = pkt[IP].src
+        ip_dst = pkt[IP].dst
+        if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
+            print (str(ip_src) + " | " + repr(pkt[DNSQR]) + " | " + " --------------> " + str(ip_dst) + " : " + "(" + str(pkt.getlayer(DNS).qd.qname) + ")") 
+        print("-->",pkt[DNS].summary())
+
+sniff(iface = interface,filter = "UDP” ,prn = querysnif, store=0)
